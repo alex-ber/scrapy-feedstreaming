2fb366172c70afb15651f56d9fc4a244362d46c5
alex-ber <toalexsmail@yahoo.com>
2020-07-02 08:43:12 +0300

    New files added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 #VERSION should be defined before importing UploadCommand
-VERSION = '0.0.1b1'
+VERSION = '0.0.1rc1'
 from alexber.utils import UploadCommand
 NAME = 'scrapy-feedstreaming'
 SHORT_NAME = 'feedstreaming'
@@ -21,7 +21,7 @@ def get_content(filename):
         content = f.read().splitlines()
     return content
 
-install_requires = get_content('requirements.txt')
+install_requires = get_content('req.txt')
 tests_require = get_content('requirements-tests.txt')
 
 extras = {
